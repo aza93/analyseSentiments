8d0e7bc4456c4b9febac6534c14ec5158c0e8b8d
Eddie Saliby <eddysaliby@gmail.com>
2021-07-07 18:25:49 +0200

    Added python script

---
+++ python/twitter.py
@@ -0,0 +1,34 @@
+import tweepy
+import re
+import io
+import csv
+
+# Credentials
+consumer_key = "Ku6mMDrwUMgTGtZro1r9cLb6W"
+consumer_secret = "agXrBFszag40NMc6bMTFF9Y3agqWL5l5HBT2rG9itIhQzAmWJe"
+access_token = "969906020330897409-lO2PhATDWh9Uxo9enuJuaPpSJtaO2RD"
+access_token_secret = "LZh4hbYdl4lsHypWVibQwoG3Ov6FnJujG9qnMezvFdjjJ"
+
+# create OAuthHandler object
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# set access token and secret
+auth.set_access_token(access_token, access_token_secret)
+# create tweepy API object to fetch tweets
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+csvFile = open('file-name', 'a')
+csvWriter = csv.writer(csvFile)
+
+print('Enter your keyword:')
+search_words = input()      # enter your words
+new_search = search_words + " -filter:retweets"
+
+count = 0
+
+for tweet in tweepy.Cursor(api.search,q=new_search,count=20,lang="en", since_id=0).items():
+    count = count + 1
+    print('tweet number', count)
+    if count <= 20:
+        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+    else:
+        break;
