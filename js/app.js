2210af29ca7e55f7f862217f202eae2eb42d567c
aza93 <a.azabar93@gmail.com>
2021-07-07 19:31:11 +0200

    Tweets fetch + bulk ES

--- js/app.js
+++ js/app.js
@@ -2,40 +2,41 @@
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: 'http://localhost:9200' })
 
-const csvFilePath = 'Data.csv'; // path du fichier csv à convertir
+const csvFilePath = '../python/data.csv'; // path du fichier csv à convertir
 const csv = require('csvtojson'); // module utilisé pour
 
 csv({
-  delimiter: ';',
+  delimiter: ',',
 	trim: false, // Désactivation de l'option de trim
 	noheader: true, // Le fichier ne contient pas de header
-  headers: ['title', 'seo_title', 'url', 'author', 'date', 'category', 'locales', 'content'], // Définition manuelle des headers
+  headers: ['date', 'description', 'username', 'location'], // Définition manuelle des headers
 })
 .fromFile(csvFilePath)
-.then(blogs => {  
-  const formattedBlogs = blogs.map((blog) => { // Je format le fichier en json
+.then(tweets => {  
+  const formattedTweets = tweets.map((tweet) => { // Je format le fichier en json
     return  {
-      title: blog['title'],
-      seo_title: blog['seo_title'],
-      url: blog['url'],
-      author: blog['author'],
-      date: blog['date'],
-      category: blog['category'],
-      locales: blog['locales'],
-      content: blog['content']
+      date: tweet['date'],
+      description: tweet['description'],
+      username: tweet['username'],
+      location: tweet['location']
     }
   })
 
+/*
+  console.log(formattedTweets);
+  process.exit(1);
+*/
+
   // Je bulk mon fichier json
-  formattedBlogs.map((formattedBlog, index) => {
+  formattedTweets.map((formattedTweet, index) => {
     //console.log('index = ', index)
-    //console.log(formattedBlog);
+    //console.log(formattedTweet);
     
     client.create({
-      index: "groupe_8", // L'index le fichier
-      type: "g8",
+      index: "groupe_5", // L'index le fichier
+      type: "g5",
       id: index,
-      body: formattedBlog
+      body: formattedTweet
     }, function(error, response) {
       if (error) {
         console.error(error);
--- python/twitter.py
+++ python/twitter.py
@@ -16,7 +16,7 @@ auth.set_access_token(access_token, access_token_secret)
 # create tweepy API object to fetch tweets
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-csvFile = open('file-name', 'a')
+csvFile = open('data.csv', 'w')
 csvWriter = csv.writer(csvFile)
 
 print('Enter your keyword:')
@@ -25,10 +25,10 @@ new_search = search_words + " -filter:retweets"
 
 count = 0
 
-for tweet in tweepy.Cursor(api.search,q=new_search,count=20,lang="en", since_id=0).items():
+for tweet in tweepy.Cursor(api.search,q=new_search,count=99,lang="en", since_id=0).items():
     count = count + 1
     print('tweet number', count)
-    if count <= 20:
-        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'),tweet.user.screen_name.encode('utf-8'), tweet.user.location.encode('utf-8')])
+    if count <= 99:
+        csvWriter.writerow([tweet.created_at, tweet.text,tweet.user.screen_name, tweet.user.location])
     else:
         break;
